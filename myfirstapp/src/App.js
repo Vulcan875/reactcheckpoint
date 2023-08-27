c7e9b7052c7bb09bd6104d2e87bb2093177b246e
Vulcan875 <borheneddine.benatttia@esprit.tn>
2023-08-27 12:34:49 +0100

    checkpoint donne

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React JS</h1>
+      </header>
+      <main className="container">
+        <p>first checkpoint</p>
+      </main>
+    </div>
+  );
+  }
+
+export default App;
